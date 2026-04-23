fcee1779d01c5e12024a7e162a452c5d6ac0fccf
ansonallard <ansononlineinfo@gmail.com>
2026-04-22 21:07:55 -0400

    fix: Remove response body and header logging
    Creates a race between this middleware and gin. Will log via the appropriate
    handler method.

--- openapi/middleware/logging.go
+++ openapi/middleware/logging.go
@@ -17,34 +17,17 @@ func InjectLogger(log *zerolog.Logger) gin.HandlerFunc {
 	}
 }
 
-type responseBodyWriter struct {
-	gin.ResponseWriter
-	body *bytes.Buffer
-}
-
-func (w *responseBodyWriter) Write(b []byte) (int, error) {
-	w.body.Write(b)
-	return w.ResponseWriter.Write(b)
-}
-
+// LoggingMiddleware - remove body/headers from response log
 func LoggingMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		log := zerolog.Ctx(c.Request.Context())
 		startTime := time.Now().UTC()
 
-		// Read and restore request body
 		var requestBody []byte
 		if c.Request.Body != nil {
 			requestBody, _ = io.ReadAll(c.Request.Body)
 			c.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
 		}
-
-		// Wrap the response writer to capture body
-		rbw := &responseBodyWriter{
-			ResponseWriter: c.Writer,
-			body:           bytes.NewBufferString(""),
-		}
-		c.Writer = rbw
 
 		log.Info().
 			Str("method", c.Request.Method).
@@ -60,8 +43,6 @@ func LoggingMiddleware() gin.HandlerFunc {
 			TimeDiff("latency", time.Now().UTC(), startTime).
 			Str("method", c.Request.Method).
 			Str("path", c.Request.URL.Path).
-			Interface("headers", c.Writer.Header()).
-			RawJSON("responseBody", rbw.body.Bytes()).
 			Msg("API Response")
 	}
 }
