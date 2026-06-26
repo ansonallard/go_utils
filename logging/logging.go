bf97bb41a09f9fb96de2df3a33d56d544bfc73e5
ansonallard <ansononlineinfo@gmail.com>
2026-06-25 20:40:04 -0400

    feat: Move logging functions, add tracing
    Move all logging functions to single module. Extend library with tracing init.

--- logging/logging.go
+++ logging/logging.go
@@ -1,11 +1,14 @@
 package logging
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"io"
 	"os"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 )
 
@@ -35,4 +38,55 @@ func ZeroLogConfiguration(logFile *os.File, logLevel *zerolog.Level, serviceName
 	// Attach the Logger to the context.Context
 	ctx = logger.WithContext(ctx)
 	return ctx
+}
+
+func InjectLogger(log *zerolog.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		reqCtx := log.WithContext(c.Request.Context())
+		c.Request = c.Request.WithContext(reqCtx)
+		c.Next()
+	}
+}
+
+// LoggingMiddleware - remove body/headers from response log
+func LoggingMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log := zerolog.Ctx(c.Request.Context())
+		startTime := time.Now().UTC()
+
+		var requestBody []byte
+		if c.Request.Body != nil {
+			requestBody, _ = io.ReadAll(c.Request.Body)
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
+		}
+
+		var incomingLog *zerolog.Event
+		if len(requestBody) > 0 && json.Valid(requestBody) {
+			var compacted bytes.Buffer
+			if err := json.Compact(&compacted, requestBody); err == nil {
+				incomingLog = log.Info().RawJSON("requestBody", compacted.Bytes())
+			} else {
+				incomingLog = log.Info().Str("requestBody", string(requestBody))
+			}
+		} else if len(requestBody) > 0 {
+			incomingLog = log.Info().Str("requestBody", string(requestBody))
+		} else {
+			incomingLog = log.Info()
+		}
+
+		incomingLog.
+			Str("method", c.Request.Method).
+			Str("path", c.Request.URL.Path).
+			Interface("headers", c.Request.Header).
+			Msg("API Request")
+
+		c.Next()
+
+		log.Info().
+			Int("status", c.Writer.Status()).
+			TimeDiff("latency", time.Now().UTC(), startTime).
+			Str("method", c.Request.Method).
+			Str("path", c.Request.URL.Path).
+			Msg("API Response")
+	}
 }
